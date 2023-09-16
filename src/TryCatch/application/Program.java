04d6c3a1b8bf3e236edb9562004d4206b00608b2
JohnDev0000 <johncroft2236@hotmail.com>
2023-09-16 09:41:50 -0300

    exercicio de exceções

--- src/TryCatch/application/Program.java
+++ src/TryCatch/application/Program.java
@@ -1,8 +1,45 @@
 package TryCatch.application;
 
+import TryCatch.model.entities.Account;
+import TryCatch.model.exceptions.WithdrawException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int accNumber = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        Double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(accNumber, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        Double amount = sc.nextDouble();
+
+        try {
+            account.withdraw(amount);
+            System.out.printf("New Balance: %.2f" + account.getBalance());
+        }
+        catch (WithdrawException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        sc.close();
     }
 }
 
